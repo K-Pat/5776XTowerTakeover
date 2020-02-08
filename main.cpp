06e0bf5c72edc4acd399d595a3d1988921f88935
K-Pat <47926388+K-Pat@users.noreply.github.com>
2020-02-07 16:19:02 -0800

    CV2 PreAuton Update
    Okapi 4 update

--- main.cpp
+++ main.cpp
@@ -25,53 +25,69 @@ pros::Controller master (CONTROLLER_MASTER);
 
 okapi::MotorGroup intakeMotor({16, -17});
 
-auto drive = okapi::ChassisControllerFactory::create(//creates an abstract motor or "Chassis Controller"
-		 {12, 13}, //left motor ports
-     {-19, -18},//right motor ports (- represents reversing ports)
-		 okapi::AbstractMotor::gearset::green,//type of gearset
-		 {3.25_in, 12.5_in}//3.25 inch wheels, 12.5 inch wheel base
- );
+auto drive = okapi::ChassisControllerBuilder()
 
- auto profileController = AsyncControllerFactory::motionProfile(
-   1,  // Maximum linear velocity of the Chassis in m/s
+		.withMotors(
+			{12,13},
+			{-19,-18}
+		)
+		 // Left motor is 1, right motor is 2 (reversed)
+    // Green gearset, 4 inch wheel diameter, 11.5 inch wheelbase
+    .withDimensions(AbstractMotor::gearset::green, {{4_in, 11.5_in}, imev5GreenTPR})
+    .withOdometry() // Use the same scales as the chassis (above)
+    .buildOdometry()
+
+;
+
+auto bot =
+  AsyncMotionProfileControllerBuilder()
+    .withLimits({1.0, 2.0, 5.0})
+    .withOutput(drive)
+    .buildMotionProfileController();
+
+
+/*auto profileController = okapi::AsyncMotionProfileControllerBuilder(
+   drive,  // Maximum linear velocity of the Chassis in m/s
    2,  // Maximum linear acceleration of the Chassis in m/s/s
    5, // Maximum linear jerk of the Chassis in m/s/s/s
    drive // Chassis Controller
- );
+ );*/
 
 
+//void move(Point target){//move function that uses okapi's 2d motion profile
+	//bot->generatePath({
+		//{0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
+		//{target}}, // The next point in the profile, 3.5 feet forward
+		//"B" // Profile name
+	//);
+		//bot->setTarget("a");
+ 	//bot->waitUntilSettled();
+//}
 
-void move(Point target){//move function that uses okapi's 2d motion profile
- 	profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, target},  "a");
- 	profileController.setTarget("a");
- 	profileController.waitUntilSettled();
+//void moveb(Point target){//move function using 2 motion profile to go backward(blocks while robot is driving)
+ 	//bot->generatePath({{0_ft, 0_ft}, target}, "a");
+	//bot.setTarget("a", true);//true inverts drive
+ 	//bot.waitUntilSettled();
+//}
 
- }
+// void FrontBlue(){}
 
- void moveb(Point target){//move function using 2 motion profile to go backward(blocks while robot is driving)
- 	profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, target}, "a");
- 	profileController.setTarget("a", true);//true inverts drive
- 	profileController.waitUntilSettled();
- }
+ //void FrontRed(){}
 
- void FrontBlue(){}
+ //void BackBlue(){
+//   intakeMotor.moveVelocity(200);
+//   move(Point{4_ft, 0_ft});
+//   drive.turnAngle(35_deg);
+//	 move(Point{1_ft, 0_ft});
+//   drive.turnAngle(90_deg);
+//   move(Point{3.8_ft, 0_ft});
+//  intakeMotor.moveVelocity(0);
+//   angler.move_velocity(-80);
+//   pros::delay(2000);
+//   moveb(Point{1_ft,0_ft});
+ //}
 
- void FrontRed(){}
-
- void BackBlue(){
-   intakeMotor.moveVelocity(200);
-   move(Point{4_ft, 0_ft, 0_deg});
-   drive.turnAngle(35_deg);
-	 move(Point{1_ft, 0_ft, 0_deg});
-   drive.turnAngle(90_deg);
-   move(Point{3.8_ft, 0_ft, 0_deg});
-  intakeMotor.moveVelocity(0);
-   angler.move_velocity(-80);
-   pros::delay(2000);
-   moveb(Point{1_ft,0_ft,0_deg});
- }
-
- void BackRed(){}
+// void BackRed(){}
 
  void Intake(){//Defines a function
  	if(master.get_digital(DIGITAL_R1) == 1){//checks to see if certain button was pressed in an if statement
@@ -86,10 +102,74 @@ void move(Point target){//move function that uses okapi's 2d motion profile
  	}
  }
 
+void TracySkills(){
+	bot->generatePath({
+	  {0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
+	  {1_ft, 0_ft, 0_deg}}, // The next point in the profile, 1 feet forward
+	  "A" // Profile name
+	);
+	bot->setTarget("A");
+	bot->waitUntilSettled();
+	lift.move_relative(2700,100);
+	while(true){
+		if(abs(lift.get_actual_velocity()) <= 0.9){
+			break;
+		}else{}
+	}
+	lift.move_relative(-1350, 100);
+	while(true){
+		if(abs(lift.get_actual_velocity()) <= 0.9){
+			break;
+		}else{}
+	}
+	bot->generatePath({
+	  {0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
+	  {6.5_ft, 0_ft, 0_deg}}, // The next point in the profile, 3.5 feet forward
+	  "B" // Profile name
+	);
+	bot->setTarget("B");
+	intakeR.move(100);
+	intakeL.move(100);
+	pros::delay(5000);
+	intakeR.move(-50);
+	intakeL.move(-50);
+	pros::delay(1000);
+	//bot->turnAngle(90_deg);
+}
+
+void TracyElimAuton(){
+	Lf.move(-100);
+	Lb.move(-100);
+	Rf.move(-100);
+	Rb.move(-100);
+	pros::delay(1500);
+	Lf.move(100);
+	Lb.move(100);
+	Rf.move(100);
+	Rb.move(100);
+	pros::delay(1500);
+	Lf.move(0);
+	Lb.move(0);
+	Rf.move(0);
+	Rb.move(0);
+}
+
+void CV2Auton(){
+	bot->generatePath({
+	  {0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
+	  {1_ft, 0_ft, 0_deg}}, // The next point in the profile, 3.5 feet forward
+	  "Z" // Profile name
+	);
+	bot->setTarget("Z", true);//reverse path to go backwards
+	bot->waitUntilSettled();
+	bot->setTarget("Z");
+	bot->waitUntilSettled();
+}
+
  void Angler(){
- 	if(master.get_digital(DIGITAL_L2) == 1){
+ 	if(master.get_digital(DIGITAL_UP) == 1){
  		angler.move(80);
- 	}else if(master.get_digital(DIGITAL_L1) == 1){
+ 	}else if(master.get_digital(DIGITAL_DOWN) == 1){
  		angler.move(-80);
  	}else{
  		angler.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);//sets a brake mode (in this case its hold)
@@ -143,16 +223,40 @@ void move(Point target){//move function that uses okapi's 2d motion profile
  }
  }
 
+void liftMacro(){
+	if(master.get_digital(DIGITAL_A)){
+		pros::delay(2500);
+		Lf.move_velocity(-100);
+		Lb.move_velocity(-100);
+		Rb.move_velocity(-100);
+		Rf.move_velocity(-100);
+		intakeL.move_velocity(-100);
+		intakeR.move_velocity(-100);
+		pros::delay(1000);
+	}else{
+		angler.move_velocity(0);
+		Lf.move_velocity(0);
+		Lb.move_velocity(0);
+		Rb.move_velocity(0);
+		Rf.move_velocity(0);
+		intakeL.move_velocity(0);
+		intakeR.move_velocity(0);
+	}
+}
+
  void Lift(){
- 	if (master.get_digital(DIGITAL_UP) == 1){
+ 	if (master.get_digital(DIGITAL_L1) == 1){
  		lift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
  		lift.move_velocity(-200);
- 	}else if(master.get_digital(DIGITAL_DOWN)){
+		angler.move(-100);
+ 	}else if(master.get_digital(DIGITAL_L2)){
  		lift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
  		lift.move_velocity(200);
+		angler.move(100);
  	}else{
  		lift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
  		lift.move_velocity(0);
+		angler.move(0);
  	}
  }
 
@@ -163,6 +267,7 @@ void move(Point target){//move function that uses okapi's 2d motion profile
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
+ //penis
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -216,7 +321,7 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-	BackBlue();
+	TracyElimAuton();
 }
 
 /**
@@ -232,7 +337,7 @@ void autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
+void opcontrol(){
 	while (true) {//An infinite while loop
     //tankDrive();
     arcadeDrive();//calling the functions
